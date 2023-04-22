ae86254524381b7550d374ad4720d7e47df16eb1
MANSI NIGAM <55457879+Mansiranjana@users.noreply.github.com>
2023-04-22 16:50:07 +0530

    Update is_rectangle.py

--- is_rectangle.py
+++ is_rectangle.py
@@ -1,3 +1,9 @@
+
+# Given four positive integers A, B, C, D, determine if there’s a rectangle such that the lengths of its sides are A, B, C and D (in any order).
+# If any such rectangle exist return 1 else return 0.
+
+
+
 class Solution:
     # @param A : integer
     # @param B : integer
